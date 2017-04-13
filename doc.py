4fd0e4318683808da89722a898c84d3cd55ba419
Yiwen Yang <381725454@qq.com>
2017-04-13 11:30:18 -0500

    Add files via upload

---
+++ doc.py
@@ -0,0 +1,79 @@
+# encoding: UTF-8
+import stemming
+
+
+class Dictionary(dict):
+
+
+    def __missing__(self, key):
+        return 0
+
+
+class Document:
+
+
+    def __init__(self, url, docID, fileName, fileType):
+
+        self.url = url
+        self.docID = docID
+        self.name = fileName
+        self.type = fileType
+        self.sName = ''
+        self.title = ''
+        self.term = Dictionary()
+
+    def setTitle(self, title):
+        '''set the title of the page'''
+
+        self.title = title
+
+    def stem(self):
+        '''implement stemming algorithm'''
+        split = self.name.split('.')
+        self.sName = split[0] + '_stem' + '.txt'
+        stemmer = stemming.PorterStemmer()
+        with open(self.name) as f:
+            while 1:
+                output = ''
+                word = ''
+                line = f.readline()
+                if line == '':
+                    break
+                for c in line:
+                    if c.isalpha():
+                        word += c.lower()
+                    else:
+                        if word:
+                            output += stemmer.stem(word, 0, len(word) - 1)
+                            word = ''
+                        output += c.lower()
+                with open(self.sName, 'a') as o:
+                    o.write(output)
+
+    def collection(self):
+        '''extract term and term frequency'''
+        with open(self.name) as f:
+            for line in f.readlines():
+                words = line.split()
+                for word in words:
+                    token = word.split('/')
+                    for t in token:
+                        t = t.rstrip(',.:-=+}])<>;!?/')
+                        t = t.lstrip(',.:-=+{[(<>')
+                        if t != '':
+                            if self.term[t] == 0:
+                                self.term[t] = 1
+                            else:
+                                self.term[t] += 1
+
+    def getTerm(self):
+        return self.term
+
+    def getID(self):
+        return self.docID
+
+    def getUrl(self):
+        return self.url
+
+    def get_title(self):
+        return self.title
